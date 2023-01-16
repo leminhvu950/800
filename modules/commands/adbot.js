cb23fabe740a8840ff39a45e21089502cebbacf6
Lê Minh Vũ <109574477+leminhvu950@users.noreply.github.com>
2023-01-16 14:34:36 +0700

    Update adbot.js

--- modules/commands/adbot.js
+++ modules/commands/adbot.js
@@ -25,7 +25,7 @@ const fs = global.nodemodule["fs-extra"];
 📍 Ho Chi Minh City
 📱 Facebook:https://www.facebook.com/anhminh.levan.1
 Này Người Đẹp Bạn Có Thể Donate cho Tôi Chứ?:3✨
-💳 VIETCOMBANK: 0721000669902
+💳 Momo: 0916155950
 `,attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg")); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.jpg")).on("close",() => callback());
    };
